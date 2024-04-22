cfb1313c6f7948261979f9528e37835f5aee4326
Alejandra Durán <79487144+Alejandraqt@users.noreply.github.com>
2024-04-22 10:27:49 -0500

    Actualización método principal
    Se agrega encapsulación y llamado a los métodos

--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,32 @@
 package com.mycompany.libropoo;
 
 public class Principal {
-    
+    /**
+     * @param args
+     *
+     * Complejidad temporal: O(1) Tiempo Constante
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String titulo = sc.nextLine();
+        String autor = sc.nextLine();
+        String genero = sc.nextLine();
+        int anioPublicacion = sc.nextInt();
+        
+        Libro miLibro = new Libro(titulo, autor, anioPublicacion, genero);
+        
+        miLibro.marcarLeido();
+        
+        System.out.println(miLibro.mostrarInformacion());
+
+        if(miLibro.isLeido()){
+            System.out.println("Ya fue leido");
+        }else{
+            System.out.println("No ha sido leido");
+        }
+        
+        if(miLibro.esAntiguo()){
+           System.out.println("Es antiguo");
+        }
+    }
 }
